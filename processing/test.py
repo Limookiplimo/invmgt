5bdf4bb25faa3f06a71cfc0e6b676ca8562d910f
Limoo Kiplimo <limoodrice@gmail.com>
2023-07-28 23:42:17 +0300

    trying different flink approaches

--- processing/test.py
+++ processing/test.py
@@ -1,52 +1,63 @@
-from pyflink.common.serialization import SimpleStringSchema
-from pyflink.common import WatermarkStrategy
+from pyflink.datastream.connectors.kafka import KafkaSink, KafkaSource,KafkaRecordSerializationSchema, DeliveryGuarantee
+from pyflink.datastream.formats.json import JsonRowSerializationSchema
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.datastream.connectors.kafka import KafkaSource, KafkaSink
+import psycopg2
+import json
+from datetime import date, time as datetime_time
 
+# Custom JSON Encoder to handle date and time objects
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, (date, datetime_time)):
+            return o.isoformat()
+        return super().default(o)
 
-env = StreamExecutionEnvironment.get_execution_environment()
-env.set_parallelism(1)
+# Extract data
+def get_data():
+    try:
+        with psycopg2.connect(
+                host='localhost',
+                port=5432,
+                database='database',
+                user='username',
+                password='password') as conn:
+            print("\n Successfully connected \n ")
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM orders;")
+            orders = cursor.fetchall()
+            cursor.close()
+        return orders
+    except psycopg2.Error as e:
+        print(f"Connection error: ", e)
+        return None
 
-# Kafka properties
-consumer_props = {
-    "bootstrap.servers": "localhost:9092",
-    "group.id": "flink_consumer_group",
-    "auto.offset.reset": "latest" 
-}
-producer_props = {
-    "bootstrap.servers": "localhost:9092"
-}
-input_topic = "orders"
-output_topic = "sales"
-schema = SimpleStringSchema()
+# Kafka producer
+def order_producer(data):
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
 
-kafka_source = KafkaSource \
+    kafka_sink = KafkaSink \
         .builder() \
         .set_bootstrap_servers("localhost:9092") \
-        .set_group_id("flink_consumer_group") \
-        .set_topics('input_topic') \
-        .set_value_only_deserializer(SimpleStringSchema()) \
+        .set_record_serializer(JsonRowSerializationSchema.builder().build()) \
+        .delivery_guarantee(DeliveryGuarantee.AT_LEAST_ONCE) \
         .build()
 
+    # Data Transformation and Sink
+    env.from_collection(data) \
+        .map(lambda x: json.dumps(x, default=str)) \
+        .sink_to(kafka_sink)
 
-# Data source
-stream = env.from_source(kafka_source, WatermarkStrategy.no_watermarks(), "Kafka Source")
+    # Execute the Flink job
+    env.execute("Write orders to Kafka")
 
-# Relevant sales data
-parsed_stream = stream \
-    .map(lambda msg: eval(msg)) \
-    .map(lambda msg: (msg["id"], msg["amount"],msg["event_time"]))
-
-# Stateful processing
-sales = parsed_stream \
-    .key_by(lambda x: x[0]) \
-    .reduce(lambda x, y: (x[0], x[1] + y[1], max(x[2], y[2])))
-
-# Print results
-sales.print()
-
-# # Data sink
-# sales.add_sink(kafka_producer)
-
-# Execute the job
-env.execute("Kafka Order Processing")
+try:
+    sales_data = get_data()
+    if sales_data:
+        order_producer(sales_data)
+        print("Data published to Kafka successfully. \n")
+    else:
+        print("No data retrieved from the database. \n")
+    
+except Exception as e:
+    print(f"Producer error: ", e)
