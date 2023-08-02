68a1021794e2e2f6445a336367e649829f095cee
Limoo Kiplimo <limoodrice@gmail.com>
2023-08-02 11:05:52 +0300

    Added fucntionality to catch and log exceptions and errors that could be encountered when reading from a topic.

--- processing/readTopic.py
+++ processing/readTopic.py
@@ -6,26 +6,29 @@ from pyflink.common import WatermarkStrategy
 import time
 
 def consume_topic():
-    env = StreamExecutionEnvironment.get_execution_environment()
-    env.set_parallelism(1)
-    bootstrap_servers = 'localhost:9092'
-    input_topic = 'orders'
-    group_id = 'flink_consumer_group'
+    try:
+        env = StreamExecutionEnvironment.get_execution_environment()
+        env.set_parallelism(1)
+        bootstrap_servers = 'localhost:9092'
+        input_topic = 'orders'
+        group_id = 'flink_consumer_group'
 
-    kafka_source = KafkaSource\
-            .builder()\
-            .set_bootstrap_servers(bootstrap_servers)\
-            .set_topics(input_topic)\
-            .set_group_id(group_id)\
-            .set_starting_offsets(KafkaOffsetsInitializer.earliest())\
-            .set_value_only_deserializer(SimpleStringSchema())\
-            .build()
-    
-    msg = env.from_source(kafka_source, WatermarkStrategy.no_watermarks(), input_topic)
+        kafka_source = KafkaSource\
+                .builder()\
+                .set_bootstrap_servers(bootstrap_servers)\
+                .set_topics(input_topic)\
+                .set_group_id(group_id)\
+                .set_starting_offsets(KafkaOffsetsInitializer.earliest())\
+                .set_value_only_deserializer(SimpleStringSchema())\
+                .build()
+        
+        msg = env.from_source(kafka_source, WatermarkStrategy.no_watermarks(), input_topic)
 
-    msg.print()
-    time.sleep(1)
-    env.execute("Read Kafka Topic")
+        msg.print()
+        time.sleep(1)
+        env.execute("Read Kafka Topic")
+    except Exception as e:
+        print(f"Topic Error: {e}")
 
 if __name__ == '__main__':
     consume_topic()
